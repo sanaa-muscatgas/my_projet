8654a955403d32289d1542dc179b8b0e540bea81
zainabA-Kahbori <hime.hihotmail.com>
2021-03-07 11:17:47 +0400

    save

---
+++ connection-example.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+
+console.log('Get connection ...');
+
+var conn = mysql.createConnection({
+	database: 'mytestdb',
+	host: "localhost",
+	user: "root",
+	password: "12345"
+});
+
+conn.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected!");
+});
